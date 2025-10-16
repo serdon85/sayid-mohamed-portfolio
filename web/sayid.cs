2af2cc0359d11dddee8177e2db5360ea293e4bfd
serdon85 <ndserdon900@gmail.com>
2025-10-16 09:49:28 +0300

    Add files via upload

---
+++ web/sayid.cs
@@ -0,0 +1,11 @@
+*{
+margin: 0;
+padding: 0;
+    box - sizing: border - box;
+     background-color: rgba(239, 12, 12, 1);
+}
+body{
+    background-color: rgba(239, 12, 12, 1);
+    font-family: Arial, Helvetica, sans-serif;
+
+}
